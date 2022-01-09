b78d1474c3a90f44a183db7ce65ae156333e3280
Bora Şavkar <borasavkar@hotmail.com>
2022-01-10 02:57:01 +0300

    commit

--- y.py
+++ y.py
@@ -668,3 +668,140 @@ if analyze_all_btn:
     st.markdown(analzye_finished, unsafe_allow_html=True)
     st.caption('İşte Sana Bugünkü Al Listem :-)')
     delete_button = st.button('Sil')
+analyze_all_btn_best = st.button('Güçlü Al Veren Hisseler ')
+if analyze_all_btn_best:
+    ticker=user_input
+    st.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    analyze_bar = st.progress(0)
+    with st.container():
+        col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+        with col1:
+            st.write('**Hisse**')
+        with col2:
+            st.write('**Öneri**')
+        with col3:
+            st.write('**Son Fiyat**')
+        with col4:
+            st.write('**Hedef**')
+        with col5:
+            st.write('**Zarar Kes**')
+        with col6:
+            st.write('**Kar Hedefi**')
+        with col7:
+            st.markdown('**Zarar Pot.**')
+    for i in tickers_all:
+        try:
+            ticker = i
+            ticker_data=yf.download(ticker,period="1y")
+            df=pd.DataFrame(ticker_data)
+            for percent_complete in range(100):
+                time.sleep(0)
+                analyze_bar.progress(percent_complete)
+            ticker_date=ticker_data.index
+            last_10_days_lastDayExcluded=ticker_date[-10:-1]
+            c=df['Close']
+            h=df['High']
+            l=df['Low']
+            last_price=c[-1]
+            maxInDate=max(h[ticker_date])
+            minInDate=min(l[ticker_date])
+            max10=max(h[last_10_days_lastDayExcluded])
+            min10=min(l[last_10_days_lastDayExcluded])
+            potentialReward=max10-last_price
+            risk=last_price-min10
+            recommendationList=['Yeni Dip Yapıyor Sat','Yeni Zirve Arayışı','AL','Sat']
+            new_low=str(recommendationList[0])
+            new_high=str(recommendationList[1])
+            buy=str(recommendationList[2])
+            dontBuy=str(recommendationList[3])
+            @st.cache
+            def tradeable():
+                if (last_price<min10):
+                    str_ticker=str("{}".format(ticker))
+                    recommendation=new_low
+                    str_lastPrice=str("{:.2f} ".format(c[-1]))
+                    str_earn_potential=''
+                    str_loss_potential=''
+                    str_target_SalePrice=''
+                    str_stopLoss=''
+                    if(minInDate<last_price):
+                        str_lastPrice=str("{:.2f} ".format(c[-1]))
+                        str_stopLoss=str("{:.2f}".format(minInDate))
+                        return str_ticker,recommendation,str_lastPrice,str_earn_potential,str_loss_potential,str_target_SalePrice,str_stopLoss
+                    return str_ticker,recommendation,str_lastPrice,str_earn_potential,str_loss_potential,str_target_SalePrice,str_stopLoss
+                elif (last_price>max10):
+                    str_ticker=str("{}".format(ticker))
+                    recommendation=new_high
+                    str_lastPrice=str("{:.2f} ".format(c[-1]))
+                    str_earn_potential=''
+                    str_loss_potential=str("{:.2f}".format(last_price-max10))
+                    str_target_SalePrice=''
+                    str_stopLoss=str("{:.2f}".format(max10))
+                    if(maxInDate>last_price):
+                        if((maxInDate-last_price)*2>(last_price-max10)):
+                            recommendation=buy
+                        else:
+                            recommendation=dontBuy
+                        str_earn_potential=str("{:.2f}".format(maxInDate-last_price))
+                        str_target_SalePrice=str("{:.2f}".format(maxInDate))
+                    return str_ticker,recommendation,str_lastPrice,str_earn_potential,str_loss_potential,str_target_SalePrice,str_stopLoss
+                else:
+                    if (potentialReward>risk*4):
+                        str_ticker=str("{}".format(ticker))
+                        recommendation=buy
+                        str_stopLoss=str("{:.2f}".format(min10))
+                        str_lastPrice=str("{:.2f} ".format(c[-1]))
+                        str_earn_potential=str("{:.2f}".format(potentialReward))
+                        str_loss_potential=str("{:.2f}".format(risk))
+                        str_target_SalePrice=str("{:.2f}".format(max10))
+                        return str_ticker,recommendation,str_lastPrice,str_earn_potential,str_loss_potential,str_target_SalePrice,str_stopLoss   
+                    else:
+                        str_ticker=str("{}".format(ticker))
+                        recommendation=dontBuy
+                        str_stopLoss=str("{:.2f}".format(min10))
+                        str_lastPrice=str("{:.2f} ".format(c[-1]))
+                        str_earn_potential=str("{:.2f}".format(potentialReward))
+                        str_loss_potential=str("{:.2f}".format(risk))
+                        str_target_SalePrice=str("{:.2f}".format(max10))
+                        str_stopLoss=str("{:.2f}".format(min10))
+                        return str_ticker,recommendation,str_lastPrice,str_earn_potential,str_loss_potential,str_target_SalePrice,str_stopLoss
+            if tradeable()[1] == buy or tradeable()[1] == new_high:
+                col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+                with col1:
+                    st.info(tradeable()[0])
+                with col2:
+                    st.success(tradeable()[1])
+                with col3:
+                    st.info(tradeable()[2])
+                with col4:
+                    st.success(tradeable()[5])
+                with col5:
+                    st.error(tradeable()[6])
+                with col6:
+                    st.success(tradeable()[3])
+                with col7:
+                    st.warning(tradeable()[4])
+            else:
+                pass
+        except ValueError:
+            st.warning('Not enough data for %s to analyze' % (ticker))
+        except MemoryError:
+            st.warning("Sorry, your device doesn't have enough memory to continue")
+        except KeyError:
+            st.warning("Key not found for %s" % (ticker))
+        except NameError:
+            st.warning(' %s not found to analyze' % (ticker))
+        except IndexError:
+            st.warning("Index for %s is out of range" % (ticker))
+        except GeneratorExit:
+            st.warning("Generator's close() method is called for %s" % (ticker))
+        except OSError:
+            st.warning("System error for %s" % (ticker))
+        except RuntimeError:
+            st.warning("Runtime error for %s" % (ticker))
+        except UnboundLocalError:
+            st.warning("No value error for %s" % (ticker))
+    analzye_finished = '<p style="font-family:Courier; color:red; font-size: 20px;">Analiz Bitti!</p>'
+    st.markdown(analzye_finished, unsafe_allow_html=True)
+    st.caption('İşte Sana Bugünkü Al Listem :-)')
+    delete_button = st.button('Sil')
